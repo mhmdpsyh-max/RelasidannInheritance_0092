abcbf59417b8778194b8d28559cfc94cf46c6237
pasyaa <mhmdpsyh@gmail.com>
2026-05-12 17:34:27 +0700

    #include "anak.h"

--- Agregasi/ibu.h
+++ Agregasi/ibu.h
@@ -1,25 +1,38 @@
 #ifndef IBU_H
 #define IBU_H
-#include <vector>
 
-class ibu {
+
+#include <iostream>
+#include <vector>
+using namespace std;
+#include "anak.h"
+
+class Ibu {
 public: 
     string nama;
     vector<anak*> daftar_anak;
 
-    ibu(string pNama) :nama(pNama) {
-        cout << "ibu \"" << nama << "\" ada\n";
-
+    Ibu(string pNama) :nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    };
+    
+    ~Ibu() {
+         cout << "Ibu \"" << nama << "\" tidak ada\n";
     }
-    ~ibu() {
-         cout << "ibu \"" << nama << "\" tidak ada\n";
-
-    }
+    
     void tambahAnak(anak*);
     void cetakAnak();
 };
-void ibu::tambahAnak(anak* pAnak) {
+
+void Ibu::tambahAnak(anak* pAnak) {
     daftar_anak.push_back(pAnak);
 }
 
-void ibu::cetakAnak(){
+void Ibu::cetakAnak(){
+ cout << "Daftar anak dari Ibu \"" << this->nama <<"\":\n";
+ for (int i = 0; i< daftar_anak.size(); i++) {
+    cout << daftar_anak[i]->nama << endl;
+   }
+   cout <<endl;
+}
+#endif
