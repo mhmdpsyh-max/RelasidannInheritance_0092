2ce335181617ad1502b7c563aee1153f4c0b870c
pasyaa <mhmdpsyh@gmail.com>
2026-05-12 17:06:06 +0700

    void ibu::cetakAnak(){

--- Agregasi/ibu.h
+++ Agregasi/ibu.h
@@ -3,4 +3,23 @@
 #include <vector>
 
 class ibu {
-    
+public: 
+    string nama;
+    vector<anak*> daftar_anak;
+
+    ibu(string pNama) :nama(pNama) {
+        cout << "ibu \"" << nama << "\" ada\n";
+
+    }
+    ~ibu() {
+         cout << "ibu \"" << nama << "\" tidak ada\n";
+
+    }
+    void tambahAnak(anak*);
+    void cetakAnak();
+};
+void ibu::tambahAnak(anak* pAnak) {
+    daftar_anak.push_back(pAnak);
+}
+
+void ibu::cetakAnak(){
