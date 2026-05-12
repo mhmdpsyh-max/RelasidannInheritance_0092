c0a2c4305fe69ad604b6bc6706241381c0bc1df1
pasyaa <mhmdpsyh@gmail.com>
2026-05-12 18:10:45 +0700

    class jantung {

--- Inheritance/inheritance.cpp
+++ Inheritance/inheritance.cpp
@@ -37,4 +37,8 @@ public:
 
 int main() {
     pelajar siswa1("andi laksono", "SMAN 1 Bantul");
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
     
+    return 0;
+}
