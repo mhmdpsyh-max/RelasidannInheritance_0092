29035484f459699ea3a25f31ecbee963bd46f5de
pasyaa <mhmdpsyh@gmail.com>
2026-05-12 17:54:42 +0700

    class pasien {

--- Agregasi/agregasi.cpp
+++ Agregasi/agregasi.cpp
@@ -5,6 +5,25 @@ using namespace std;
 #include "ibu.h"
 
 int main() {
-    ibu* varIbu1 = new ibu("dini");
-    ibu* varIbu2 = new ibu("novi");
-    
+    Ibu* varIbu1 = new Ibu("dini");
+    Ibu* varIbu2 = new Ibu("novi");
+    anak* varAnak1 = new anak("tono");
+    anak* varAnak2 = new anak("rini");
+    anak* varAnak3 = new anak("dewi");
+
+    varIbu1->tambahAnak(varAnak1);
+    varIbu1->tambahAnak(varAnak2);
+    varIbu2->tambahAnak(varAnak3);
+    varIbu2->tambahAnak(varAnak1);
+
+    varIbu1->cetakAnak();
+    varIbu2->cetakAnak();
+
+    delete varIbu1;
+    delete varIbu2;
+    delete varAnak1;
+    delete varAnak2;
+    delete varAnak3;
+
+    return 0;
+}
