0ba665da6016d29ec47e7ad1eef224aac665b12f
pasyaa <mhmdpsyh@gmail.com>
2026-05-12 18:45:30 +0700

    User(string pNama, string pEmail)

--- Tugas.cpp
+++ Tugas.cpp
@@ -2,5 +2,14 @@
 #include <string>
 using namespace std;
 
+// pembuatan class user
 class User {
 protected:
+    static int globalId;
+    int id;
+    string nama;
+    string email;
+
+public:
+  User(string pNama, string pEmail)
+  
