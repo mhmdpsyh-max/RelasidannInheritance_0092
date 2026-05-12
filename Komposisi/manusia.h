56809c47d0fd35fd76623f1befa85e67c8015302
pasyaa <mhmdpsyh@gmail.com>
2026-05-12 18:15:53 +0700

    cout << name << "mati\n";

--- Komposisi/manusia.h
+++ Komposisi/manusia.h
@@ -1,1 +1,15 @@
 #ifndef MANUSIA_H
+#define MANUSIA_H
+using namespace std;
+
+class manusia {
+public :
+    string name;
+    jantung varJantung;
+
+    manusia(string pName)
+        : name(pName) {
+        cout << name << " hidup\n";
+    }
+    ~manusia() {
+        cout << name << "mati\n";
