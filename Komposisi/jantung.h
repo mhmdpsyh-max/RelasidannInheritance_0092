d7a5759545768912769e8a4089bb4c23fbcc8314
pasyaa <mhmdpsyh@gmail.com>
2026-05-12 18:15:15 +0700

    #ifndef MANUSIA_H

--- Komposisi/jantung.h
+++ Komposisi/jantung.h
@@ -1,10 +1,16 @@
 #ifndef JANTUNG_H
 #define JANTUNG_H
 using namespace std;
+#include <iostream>
 
 class jantung {
 public:
     jantung(){
         cout << "jantung dihidupkan\n";
     }
-}
+
+    ~jantung() {
+        cout << "jantung dimatikan\n";
+    }
+};
+#endif
---
+++ Komposisi/manusia.h
@@ -0,0 +1,1 @@
+#ifndef MANUSIA_H
